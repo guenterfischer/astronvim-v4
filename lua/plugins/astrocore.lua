f9abdc77012d983dfa665c353a2874483a1978cd
Günter Fischer <fischer-guenter@gmx.net>
2024-04-13 14:36:57 +0200

    Added mapping to enter normal mode in terminal with ESC

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -26,28 +26,28 @@ return {
     options = {
       opt = { -- vim.opt.<key>
         relativenumber = false, -- sets vim.opt.relativenumber
-        number = true,          -- sets vim.opt.number
-        spell = false,          -- sets vim.opt.spell
-        signcolumn = "auto",    -- sets vim.opt.signcolumn to auto
-        wrap = false,           -- sets vim.opt.wrap
-        encoding = "utf-8",     -- sets vim.opt.encoding
-        fileformats = "unix",   -- sets vim.opt.fileformats
-        tabstop = 2,            -- sets vim.opt.tabstop
-        shiftwidth = 2,         -- sets vim.opt.shiftwidth
-        expandtab = true,       -- sets vim.opt.expandtab
-        autoindent = true,      -- sets vim.opt.autoindent
-        shell = "/usr/bin/bash" -- sets vim.opt.shell
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        wrap = false, -- sets vim.opt.wrap
+        encoding = "utf-8", -- sets vim.opt.encoding
+        fileformats = "unix", -- sets vim.opt.fileformats
+        tabstop = 2, -- sets vim.opt.tabstop
+        shiftwidth = 2, -- sets vim.opt.shiftwidth
+        expandtab = true, -- sets vim.opt.expandtab
+        autoindent = true, -- sets vim.opt.autoindent
+        shell = "/usr/bin/bash", -- sets vim.opt.shell
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
-        mapleader = " ",                 -- sets vim.g.mapleader
-        autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-        cmp_enabled = true,              -- enable completion at start
-        autopairs_enabled = true,        -- enable autopairs at start
-        diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-        icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+        mapleader = " ", -- sets vim.g.mapleader
+        autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+        cmp_enabled = true, -- enable completion at start
+        autopairs_enabled = true, -- enable autopairs at start
+        diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+        icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
         ui_notifications_enabled = true, -- disable notifications when toggling UI elements
       },
     },
@@ -80,6 +80,7 @@ return {
       t = {
         -- setting a mapping to false will disable it
         -- ["<esc>"] = false,
+        ["<esc>"] = { "<C-\\><C-n>", desc = "Switch to normal mode" },
       },
     },
   },
