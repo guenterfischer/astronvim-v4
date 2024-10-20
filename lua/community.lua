78db4469e6c29439a26345003a86234705743ac1
Günter Fischer <fischer-guenter@gmx.net>
2024-10-20 11:32:37 +0000

    feat: Removed ruby tooling

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,6 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
 }
